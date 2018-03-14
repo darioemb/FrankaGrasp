5ef4a152a3d0eed980da66a20442a9ac2ecf39fc
sphero <sphero@outlook.it>
2018-03-14 13:41:46 +0100

    Improved script configure.

--- configure.sh
+++ configure.sh
@@ -1,3 +1,22 @@
 CURR=$PWD
 
+echo -e "\ee[33mInstalling panda-moveit-config dependencies\e[39m"
+sudo apt-get install ros-kinetic-panda-moveit-config
+echo -e "\e[33mInstalled dependencies.\e[39m"
+
+echo -e "\e[33mCOMPILING libfranka...\e[39m"
+cd libfranka
+if [ -d "lib/libfranka/build" ]; then
+	echo -e "\e[33mDirectory build found. Clean your workspace or leave so it is and continue manually!!\e[39m"
+else
+	cd lib/libfranka
+	mkdir build
+	cd build
+
+	cmake -DCMAKE_BUILD_TYPE=Release ..
+	cmake --build .
+
+	echo -e "\e[32m######### libfranka compiled correctly!\e[39m"
+fi
+
 catkin_make -DCMAKE_BUILD_TYPE=Release -DFranka_DIR:PATH=$CURR/lib/libfranka/build
