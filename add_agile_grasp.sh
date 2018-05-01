51cf69652342e99199f11bdb021d465527cc2f04
sphero <sphero@outlook.it>
2018-05-01 10:42:57 +0200

    Deleted old wrong version

--- add_agile_grasp.sh
+++
@@ -1,3 +0,0 @@
-cp agile_grasp_sources/franka_grasp.cpp src/agile_grasp/src/nodes/
-cp agile_grasp_sources/panda_localizer.cpp src/agile_grasp/src/agile_grasp/
-cp agile_grasp_sources/panda_localizer.h src/agile_grasp/include/agile_grasp/
