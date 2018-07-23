fea5135da46968dd22a5030b1ad8701b98d758fc
Dave <zor.dav.20392@gmail.com>
2018-07-23 14:39:55 +0200

    .gitignore now working

--- doc/graspdemounknownobjects/grasping_rectangle/runnew.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-#rostopic pub /SS/doSingleShot std_msgs/String "asdf" -1
-`rospack find grasping_rectangle`/bin/rank /tmp/scene0001r.png /tmp/background.png /tmp/scene0001.txt `rospack find grasping_rectangle`/model/model.txt /tmp/
--- doc/graspdemounknownobjects/grasping_rectangle/src/indent.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-indent -bap -bli0 -i2 -l79 -ncs -npcs -npsl -fca -lc79 -fc1 -nut -ts2 $1
